b5ac358386234e262afc77620e4974e5d5013bd1
Joshua Winters-Brown <117940901+ofgrenudo@users.noreply.github.com>
2023-05-12 13:05:16 -0400

    Currently can pull assets from SnipeIT with a Serial Number, as well as handle errors etc.

---
+++ docs/resources.md
@@ -0,0 +1,5 @@
+Returning Objects - https://www.wapshere.com/missmiis/returning-an-object-from-a-powershell-function
+Creating Objects - https://stackoverflow.com/questions/644772/structs-or-objects-in-powershell-2
+Getting Device Information - https://www.powershellgallery.com/packages/PSDellWarranty/0.1.0/Content/Public%5CGet-DellAssetInfo.ps1
+Creating environment hasmap - https://stackoverflow.com/questions/72236557/how-do-i-read-a-env-file-from-a-ps1-script
+Dealing with Json in Powershell - https://devblogs.microsoft.com/scripting/working-with-json-data-in-powershell/
--- src/ponerlo.ps1
+++ src/ponerlo.ps1
@@ -1,4 +1,20 @@
 <#
+    Ignore any untrusted ssl/tls certs.
+#>
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+<#
     Get-DeviceInformation indexes your computer and generates a object that contians
         - computer_name (Hostname)
         - make (The devices Manufacturer)
@@ -36,3 +52,81 @@ function Test-GetDeviceInformation {
     Write-Host $device.serial_number
     Write-Host $device.asset_tag
 }
+
+<#
+    This function will read through an existing .env file and return a hashmap containing each of the values present
+    in the .env file.
+#>
+function Get-EnvironmentVariables { 
+    $environment_variables = @{}
+
+    Get-Content .env | foreach {
+        $name, $value = $_.split('=')
+        if ([string]::IsNullOrWhiteSpace($name) -Or $name.Contains('#')) {
+            continue
+        }
+        $environment_variables.add($name, $value)
+    }
+    return $environment_variables
+}
+
+<#
+    This function will go through the .env hashtable returned and print each of the values out to the console.
+#>
+function Test-GetEnvironmentVariables {
+    $my_env = Get-EnvironmentVariables
+    foreach ($sub_env in $my_env.GetEnumerator() ) {
+        Write-Host "$($sub_env.Name) : $($sub_env.Value)"
+    }
+}
+
+<#
+    This device will make a get request to your SnipeIT infrastructure point and will query for a devcie with the provided serial number.
+    if the device does exists. It will return an object, with existing information from the Snipe Inventory. If the device does not exist, 
+    it will return, a error value.
+    
+    Snipe-IT API Documentation: https://snipe-it.readme.io/reference/hardware-by-serial
+#>
+function Get-DeviceFromSnipeWithSN([string]$serial_number) {
+    $my_env = Get-EnvironmentVariables
+
+    $headers=@{}
+    $headers.Add("accept", "application/json")
+    $headers.Add("Authorization", "Bearer " + $my_env.snipe_api_key)
+
+    $uri = $my_env.snipe_root_url + "api/v1/hardware/byserial/" + $serial_number + "?deleted=false"
+ 
+    $response = Invoke-WebRequest -Uri $uri -Method GET -Headers $headers
+    $response = $response | ConvertFrom-Json  
+
+    return $response
+}
+
+function Test-GetWithBadSN {
+    $response_from_snipe = Get-DeviceFromSnipeWithSN("234215131242")
+    if ($response_from_snipe.status) { Write-Host $response_from_snipe.messages }
+}
+
+function Test-GetWithGoodSN {
+    $my_device = Get-DeviceInformation
+    $response_from_snipe = Get-DeviceFromSnipeWithSN($my_device.serial_number)
+    if ($response_from_snipe.status) { Write-Host $response_from_snipe.messages }
+
+    $response_from_snipe.rows[0] | ft
+}
+
+function Get-DeviceFromSnipeWithAT {
+    return "Need To Implement."
+}
+
+function Test-AllFunctions { 
+    Test-GetEnvironmentVariables
+    Write-Host("="*100)
+    Test-GetDeviceInformation
+    Write-Host("="*100)
+    Test-GetWithBadSN
+    Write-Host("="*100)
+    Test-GetWithGoodSN    
+}
+
+Test-AllFunctions
